07ca735525ea910c1abcf9b04ff71718121663c0
JGamrot <145107244+Jgamrot@users.noreply.github.com>
2024-04-13 20:02:00 +0100

    Pass all tests
    Had some challenge with the ands and the ors operators.
    I had them the wrong way around.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,31 @@
-const leapYears = function() {
+const leapYears = function(enteredYear) {
 
+    if (enteredYear % 4 === 0 && (enteredYear % 100 !== 0 || enteredYear % 400 === 0 )){
+        return true;
+    } else {
+        return false;
+    } 
 };
+console.log(leapYears(2000));
+console.log(leapYears(1985));
+console.log(leapYears(1996));
+console.log(leapYears(1900));
+
+
+
+/*
+
+leapYears(2000) // is a leap year: returns true
+leapYears(1985) // is not a leap year: returns false
+
+
+Create a function that determines whether or not a given year is a leap year. 
+Leap years are determined by the following rules:
+
+> Leap years are years divisible by four
+However, years divisible by 100 are not leap years 
+unless they are divisible by 400
+*/
 
 // Do not edit below this line
 module.exports = leapYears;
