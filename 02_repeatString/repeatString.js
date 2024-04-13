7b2c7476201dbd7e6c89c2d86e259e7f8e929780
JGamrot <145107244+Jgamrot@users.noreply.github.com>
2024-04-13 12:02:20 +0100

    Pass all tests
    written code to pass the tests and removed
    .skip from all in the .spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(givenString, numberOfRepeats) {
+    let returnedString = "";
+    if (numberOfRepeats >= 0) {
+for (let x=1; x <= numberOfRepeats ; x++){
+    returnedString += givenString;
+}
+return returnedString;
 
+    }    else { return "ERROR"}
 };
+
+const result = repeatString ( "hey", 3);
+console.log(result);
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
