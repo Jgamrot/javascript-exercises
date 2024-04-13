38102fc5ae3ba928bfced2d868a54ea814eadc06
JGamrot <145107244+Jgamrot@users.noreply.github.com>
2024-04-13 14:26:40 +0100

    Wroks until test 3
    Doesn't work with larger numbers first. Using
    commit message to show my progress

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,45 @@
-const sumAll = function() {
-
+const sumAll = function(intA, intB) {
+    let finalSum = 0;
+    for (let x = intA; x <= intB; x++){
+        finalSum += x
+    }
+return finalSum;
 };
+
+
+const result = sumAll ( 1, 4);
+console.log(result);
+/*input is two variables x y
+use a loop
+i=x, x = y, x++
+start with the value of the first int
+loop until the we get to the last variable (y)
+loop by 1 increment each time
+
+for each loop
+let final sum = x
+then take the value of x, and add to final sum
+
+0, + 1 = 1
+1 + 2 = 3
+3 + 3 = 6
+6 + 4 = 10
+
+
+
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+```
+
+## Hints
+
+Think about how you would do this on pen and paper and then how you might translate that process into code:
+- make sure you pay attention to the function parameters
+- create a variable to hold the final sum
+- loop through the given numbers ([link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration))
+- on each iteration add the number to the sum
+- return the sum after finishing the loop
+
+*/
 
 // Do not edit below this line
 module.exports = sumAll;
