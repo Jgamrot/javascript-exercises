868d10c6890630d8ce0ff184e620adca30d1e9e7
JGamrot <145107244+Jgamrot@users.noreply.github.com>
2024-04-13 13:49:51 +0100

    First attempt
    Passeds the first test, but doesn't filter out multiple numbers.
    Using google & Claude to learn about .filter methods.
    
    I had used splice but it didn't work. splice was interpreting the index
    not the value to remove. So looked into .filter instead.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(enteredArray, argumentToRemove) {
+   let newArray = enteredArray.filter(function(itemInArray) {
+    return itemInArray !== argumentToRemove;
+});
+return newArray;
 };
+
+const result = removeFromArray([1, 2, 3, 4], 3);
+console.log(result);
 
 // Do not edit below this line
 module.exports = removeFromArray;
