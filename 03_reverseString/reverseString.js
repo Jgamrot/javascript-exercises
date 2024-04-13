7574d7cdfe3de38fde3a765c106abf5c9342dfbf
JGamrot <145107244+Jgamrot@users.noreply.github.com>
2024-04-13 12:40:02 +0100

    Pass all tests
    Written code, passes test.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let myArray = Array.from(string);
+    myArray.reverse();
+    let newString = myArray.join("");
+
+    return newString;
 
 };
+
+const result = reverseString ( "Hello there");
+console.log(result);
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
